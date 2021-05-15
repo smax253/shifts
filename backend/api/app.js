76ed7673c6792a0308d05a20d457c8bc82543584
Hamzah Nizami <hnizami1@stevens.edu>
2021-05-15 00:13:31 -0400

    Modify API to pull from several resources plus implement normalization to offset hype stocks (#16)
    * Adds the API. [Resolves #7]
    
    * Modified the API to pull from multiple reddit sources + normalized. Resolves #8

---
+++ backend/api/app.js
@@ -0,0 +1,142 @@
+//Originally implemented by andrews1022.
+//Original source code found here: https://github.com/andrews1022/reddit-stock-scraper
+//Modified by Hamzah Nizami
+// All credit goes to original implementer
+
+// packages
+import cheerio from 'cheerio';
+import fetch from 'node-fetch';
+
+// api
+import BASE_URL from './api/api';
+
+// data
+import ERROR_MESSAGE from './data/errorMessage';
+import invalidTickers from './data/invalidTickers';
+
+// functions
+import getPostHtml from './functions/getPostHtml';
+
+const normalizeMentionsCount = (denoms, allSubTickers) => {
+	let minimum = Math.min.apply(Math, denoms) / 100000; //calculate the minimum and scale it down by a factor of 100,000 (lets work with smaller numbers)
+	for(let i = 0; i < allSubTickers.length; i++){ 
+		for(let stockObj of allSubTickers[i]){
+			stockObj.timesCounted *= minimum;
+			stockObj.timesCounted /= (denoms[i] / 100000); //apply the proportion
+		}
+	}
+
+	return allSubTickers	
+}
+
+const twoArrayObjectCombine = (arr1, arr2) => {
+	const res = Object.values([...arr1, ...arr2].reduce((acc, { stock, timesCounted  }) => {
+		if (acc[stock]) acc[stock].timesCounted  += parseInt(timesCounted);
+		else acc[stock] = { stock, timesCounted : parseInt(timesCounted) };
+		return acc;
+	  }, {}));
+	  return res;
+}
+
+const combineResults = (modifiedCountTickers) => {
+	if(modifiedCountTickers.length === 1){ 
+		return modifiedCountTickers[0]
+	}
+
+	while(modifiedCountTickers.length !== 1) { 
+		let firstArrayObj = twoArrayObjectCombine(modifiedCountTickers[0], modifiedCountTickers[1]);
+		modifiedCountTickers.splice(1, 1);
+		modifiedCountTickers[0] = firstArrayObj
+	}
+	//console.log(modifiedCountTickers)
+	return modifiedCountTickers;
+
+}
+
+//take in a list of strings of subreddits to query
+const scrapeReddit = async (allSubreddits, topXTickers) => {
+	// start user prompt/input
+	let denominators = [];
+	try {
+		let allSubResults = [];
+		
+		for (let i = 0; i < allSubreddits.length; i++) {
+			// fetch subreddit page html
+			const response = await fetch(`${BASE_URL}/r/${allSubreddits[i]}/`);
+			const html = await response.text();
+
+			// initialize cheerio
+			const $ = cheerio.load(html);
+
+			// grab all hrefs
+			const hrefs = [];
+			$('.thing .entry .top-matter .title a.title').each((i, el) => {
+				hrefs[i] = $(el).attr('href');
+			});
+
+			const subscriberCount = $('.number');
+			const subscribersAndActive = subscriberCount.text().split(',');
+			let subscribers = "";
+			for(let j = 0; j < subscribersAndActive.length; j++){ 
+				if(subscribersAndActive[j].length > 3){ 
+					let endOfFirstNum = subscribersAndActive[j].slice(0, 3);
+					subscribersAndActive[j] = subscribersAndActive[j].slice(3)
+					subscribersAndActive.splice(j, 0, endOfFirstNum);
+					subscribers += subscribersAndActive[j]
+					break;
+				}
+				subscribers += subscribersAndActive[j]
+			}
+			subscribers = parseInt(subscribers);
+			denominators.push(subscribers);
+			// filter the hrefs - only get hrefs that include the subreddit name (avoids links for ads)
+			const filteredHrefs = hrefs.filter((href) => href.includes(`/r/${allSubreddits[i]}`));
+			// call the getPostHtml fn
+			const parsedHtml = await getPostHtml(filteredHrefs);
+
+			// ticker regex
+			const regex = /\$?\b[A-Z]{1,4}\b/g;
+
+			// extract all tickers matching the regex
+			// this is including duplicates, as we want to count them later
+			const tickers = parsedHtml
+				.match(regex)
+				.sort()
+				.map((ticker) => ticker.replace('$', ''))
+				.filter((ticker) => invalidTickers.indexOf(ticker) < 0);
+
+			// object structure {stock: 'TICKER_NAME_HERE', timesCounted: 1'}
+			const countedTickers = [];
+
+			tickers.forEach((ticker) => {
+				// if not found...
+				if (!countedTickers.filter((match) => match.stock === ticker).length) {
+					// add it to array
+					countedTickers.push({ stock: ticker, timesCounted: 1 });
+				} else {
+					// if already in the array, just increase timesCounted by 1
+					countedTickers.find((dupe) => dupe.stock === ticker).timesCounted += 1;
+				}
+			});
+
+			// sort tickers by timesCounted (highest to lowest)
+			const sortedTickers = countedTickers.sort((tickerA, tickerB) =>
+				tickerA.timesCounted < tickerB.timesCounted ? 1 : -1
+			);
+
+			const topTickers = sortedTickers.splice(0, topXTickers);
+			allSubResults.push(topTickers);
+		}
+		let updatedCounts = normalizeMentionsCount(denominators, allSubResults)
+		return combineResults(updatedCounts)
+
+	} catch (error) {
+		if (error) console.log(ERROR_MESSAGE);
+	}
+};
+
+//scrapeReddit(["wallstreetbets", "stocks", "investing", "pennystocks"], 10); 
+
+module.exports = { 
+	scrapeReddit
+}
---
+++ backend/api/data/invalidTickers.js
@@ -0,0 +1,103 @@
+const invalidTickers = [
+	'A',
+	'G',
+	'I',
+	'N',
+	'P',
+	'Q',
+	'S',
+	'X',
+	'AP',
+	'CA',
+	'DD',
+	'EV',
+	'GO',
+	'IE',
+	'IN',
+	'JC',
+	'MF',
+	'TO',
+	'UN',
+	'US',
+	'WS',
+	'BNN',
+	'CAD',
+	'CSE',
+	'DIY',
+	'DFV',
+	'DMV',
+	'ETF',
+	'IPO',
+	'IRL',
+	'LET',
+	'LIF',
+	'LOL',
+	'RIF',
+	'RSS',
+	'TSX',
+	'UTC',
+	'WSB',
+	'WTF',
+	'WTH',
+	'AQAA',
+	'APES',
+	'BACK',
+	'DRIP',
+	'FOMO',
+	'FUCK',
+	'HERE',
+	'HODL',
+	'HOLD',
+	'JSON',
+	'LIRA',
+	'LMAO',
+	'NSFW',
+	'NYSE',
+	'OVER',
+	'REIT',
+	'RESP',
+	'RRSP',
+	'SHIT',
+	'TFSA',
+	'VOTE',
+	'YOLO'
+];
+
+export default invalidTickers;
+
+// ----- SINGLE LETTER STOCKS -----
+// A - N/A
+// B - Barnes Group Inc.
+// C - Citigroup Inc.
+// D - Dominion Energy, Inc.
+// E - Eni S.p.A.
+// F - Ford Motor Company
+// G - N/A
+// H - Hyatt Hotels Corporation
+// I - N/A
+// J - Jacobs Engineering Group Inc.
+// K - Kellogg Company
+// L - Loews Corporation
+// M - Macy's, Inc.
+// N - N/A
+// O - Realty Income Corporation
+// P - N/A
+// Q - N/A
+// R - Ryder System, Inc.
+// S - N/A
+// T - AT&T Inc.
+// U - Unity Software Inc.
+// V - Visa Inc.
+// W - Wayfair Inc.
+// X - United States Steel Corporation
+// Y - Alleghany Corporation
+// Z - Zillow Group, Inc.
+
+// ----- COMMON TERMS THAT HAPPEN TO BE STOCKS (SEARCH ON YOUR OWN) -----
+// Format: TICKER - STOCK/ETF/etc.
+
+// DD - DuPont de Nemours, Inc.
+// EV - Eaton Vance Corp.
+// GO - Grocery Outlet Holding Corp.
+// IPO - Renaissance IPO ETF
+// YOLO - AdvisorShares Pure Cannabis ETF
